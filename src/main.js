e621225338ad05c3281973ad5592cce0bad72509
DevDaru <devvmarko@gmail.com>
2026-02-06 19:54:47 +0100

    complete mobile version

--- src/main.js
+++ src/main.js
@@ -1,24 +1,16 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+const btnHamburger = document.querySelector('.toggle-btn');
+const mobileMenu = document.querySelector('.mobile-menu');
+const body = document.querySelector('body');
+const icon = btnHamburger.querySelector('img');
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
-
-setupCounter(document.querySelector('#counter'))
+btnHamburger.addEventListener('click', () => {
+  if (mobileMenu.classList.contains('open')) { // Close Menu
+    mobileMenu.classList.remove('open');
+    body.classList.remove('noscroll');
+    icon.src = './assets/icon-hamburger.svg'; // Correct path relative to index.html/root
+  } else { // Open Menu
+    mobileMenu.classList.add('open');
+    body.classList.add('noscroll');
+    icon.src = './assets/icon-close.svg';
+  }
+});
