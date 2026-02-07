01c4d91bfeb57a884ed77c19f3b9315d18785b91
DevDaru <devvmarko@gmail.com>
2026-02-07 17:14:57 +0100

    fix hamburger button

--- src/main.js
+++ src/main.js
@@ -7,10 +7,10 @@ btnHamburger.addEventListener('click', () => {
   if (mobileMenu.classList.contains('open')) { // Close Menu
     mobileMenu.classList.remove('open');
     body.classList.remove('noscroll');
-    icon.src = './assets/icon-hamburger.svg'; // Correct path relative to index.html/root
+    icon.src = '../assets/icon-hamburger.svg'; // Correct path relative to index.html/root
   } else { // Open Menu
     mobileMenu.classList.add('open');
     body.classList.add('noscroll');
-    icon.src = './assets/icon-close.svg';
+    icon.src = '../assets/icon-close.svg';
   }
 });
